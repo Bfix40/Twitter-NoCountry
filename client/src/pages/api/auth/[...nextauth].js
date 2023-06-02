fd8a5920cdca01e62ad4174bec2702d3948aad52
Cristian <86857081+illiCristian@users.noreply.github.com>
2023-06-01 23:39:21 -0300

    Update [...nextauth].js

--- client/src/pages/api/auth/[...nextauth].js
+++ client/src/pages/api/auth/[...nextauth].js
@@ -26,6 +26,17 @@ export const authOptions = {
     // ...add more providers here
   ],
   callbacks: {
+    useSecureCookies: true,
+    cookies: {
+      callbackUrl: {
+        name: '__Secure-next-auth.callback-url',
+        options: {
+          sameSite: 'lax',
+          path: 'https://absorbing-record-production.up.railway.app/',
+          secure: true
+        }
+      }
+    },
     async session({ session, user }) {
       session.user.name = user.name;
       session.user._id = user.id;
