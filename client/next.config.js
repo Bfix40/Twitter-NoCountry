6e84a90ab8d023d720647111de835395e657ce25
Cristian <86857081+illiCristian@users.noreply.github.com>
2023-06-02 20:19:36 -0300

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-    cookieDomain: 'https://twitter-nocountry-production.up.railway.app/home'
+    cookieDomain: 'https://twitter-nocountry-production.up.railway.app/home',
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
