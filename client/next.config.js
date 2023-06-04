3d321d788f9dd44e11ffb7d25306771065f52c83
Billy <fx4038@gmail.com>
2023-06-03 22:16:27 -0400

    fix: resolving deploy #11

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  }
-}
+    cookieDomain: 'twitter-nocountry-production.up.railway.app',
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
+};
